6c648154382ba91e49e0e5e9f61803e37dcbc947
kwokster10 <kwokster10@hotmail.com>
2015-04-12 14:38:45 -0400

    added users creation page

--- blog.js
+++ blog.js
@@ -10,9 +10,9 @@ var db = new sqlite3.Database("blog.db");
 
 // creating the tables sequentially in the js rather than have sep schema and seed files
 db.serialize(function() {
-	db.run("CREATE TABLE IF NOT EXISTS backpackers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, image TEXT, background TEXT, body TEXT);");
-	db.run("CREATE TABE IF NOT EXISTS posts (p_id INTEGER, p_body TEXT, p_image TEXT); CREATE TRIGGER timestamp_update BEFORE UPDATE ON posts BEGIN UPDATE posts SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id;");
-	db.run("CREATE TABLE IF NOT EXISTS comments (c_id INTEGER, c_name TEXT, c_body TEXT); CREATE TRIGGER timestamp_update BEFORE UPDATE ON comments BEGIN UPDATE comments SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id;");
+	db.run("CREATE TABLE IF NOT EXISTS backpackers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, image TEXT, background TEXT, info TEXT);");
+	db.run("CREATE TABE IF NOT EXISTS posts (p_id INTEGER PRIMARY KEY, p_body TEXT, p_image TEXT, b_id INTEGER); CREATE TRIGGER timestamp_update BEFORE UPDATE ON posts BEGIN UPDATE posts SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id;");
+	db.run("CREATE TABLE IF NOT EXISTS comments (c_id INTEGER PRIMARY KEY, c_name TEXT, c_body TEXT, p_id INTEGER); CREATE TRIGGER timestamp_update BEFORE UPDATE ON comments BEGIN UPDATE comments SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id;");
 });
 
 // redirecting to my main page
@@ -22,9 +22,23 @@ app.get("/", function(req, res){
 
 // my main page; making this a blog for backpackers with things to say
 app.get("/backpackers", function(req, res){
-	db.all("SELECT * FROM backpackers;", function(err){
-		res.render("index.ejs");
+	db.all("SELECT * FROM backpackers;", function(err, rows){
+		res.render("index.ejs", {backpackers : rows});
 	});
+});
+
+app.get("/backpackers/new", function(req, res) {
+	res.render("new.ejs");
+});
+
+app.post("/backpackers", function(req, res) {
+	db.run("INSERT INTO backpackers (name, image, background, info) VALUES (?, ?, ?, ?);", req.body.name, req.body.image, req.body.background, req.body.info, function(err) {
+		if (err) {
+			throw err;
+		} else {
+			res.redirect("/backpackers");
+		}
+	})
 });
 
 
